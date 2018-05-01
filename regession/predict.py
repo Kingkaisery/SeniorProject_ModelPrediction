35af9380f5a9e00a41eaefc7e4e798bdd05e933a
Kingkaisery <nitisit.j@ku.th>
2018-05-01 00:43:58 -0700

    improve model

--- regession/predict.py
+++ regession/predict.py
@@ -34,31 +34,58 @@ def TokenizingTestData(data,start,end,vect,vect2):
 	tests = hstack((des,title))
 	return tests
 
-def getTestData(data,start,end,vect,vect2):
+def getTestData(data,start,end,vect,vect2,vect3,vect4,vect5,vect6,vect7,vect8):
 	targets = []
 	desTests = []
 	titleTests = []
+	levelTests = []
+	orgTests = []
+	loTests = []
+	eduTests = []
+	expTests = []
+	indTests = []	
+	jobfuncTests = []
 	for i in xrange(start,end):
 		job = data[i].data
-		desTests.append(job['level'] + ' ' + job['organization'] + ' ' + job['joblocation'] + ' ' + job['education'] + ' ' + 
-		job['experience'] + ' ' + job['employmentType'] + ' ' + job['industry'] + ' ' + job['jobfunction'])
 		titleTests.append(job['title'])
-	title = vect.transform(titleTests)
-	des = vect2.transform(desTests)
-	tests = hstack((title,des))
+		levelTests.append(job['level'])
+		orgTests.append(job['organization'])
+		loTests.append(job['joblocation'])
+		eduTests.append(job['education'])
+		expTests.append(job['experience'])
+		indTests.append(job['industry'])
+		jobfuncTests.append(job['jobfunction'])
+		#desTests.append(job['level'] + ' ' + job['organization'] + ' ' + job['joblocation'] + ' ' + job['education'] + ' ' + 
+		#job['experience'] + ' ' + job['employmentType'] + ' ' + job['industry'] + ' ' + job['jobfunction'])		
+	title = vect1.transform(titleTests)
+	level = vect2.transform(levelTests)
+	org = vect3.transform(orgTests)
+	lo = vect4.transform(loTests)
+	edu = vect5.transform(eduTests)
+	exp = vect6.transform(expTests)
+	ind = vect7.transform(indTests)
+	jobfunc = vect8.transform(jobfuncTests)
+	#des = vect2.transform(desTests)
+	tests = hstack((title,level,org,lo,edu,exp,ind,jobfunc))
 	return tests
 
 paths = json.load(open('SETTINGS.json','rb'))
 print 'Loading models'
 model = joblib.load(paths['MODEL_PATH'])
-vect = joblib.load(paths['VECTORIZER_1_PATH'])
+vect1 = joblib.load(paths['VECTORIZER_1_PATH'])
 vect2 = joblib.load(paths['VECTORIZER_2_PATH'])
+vect3 = joblib.load(paths['VECTORIZER_3_PATH'])
+vect4 = joblib.load(paths['VECTORIZER_4_PATH'])
+vect5 = joblib.load(paths['VECTORIZER_5_PATH'])
+vect6 = joblib.load(paths['VECTORIZER_6_PATH'])
+vect7 = joblib.load(paths['VECTORIZER_7_PATH'])
+vect8 = joblib.load(paths['VECTORIZER_8_PATH'])
 
 print 'loading testing data',paths['TEST_DATA_PATH']
 validData = LoadData(paths['TEST_DATA_PATH'])
 titles = [validData[i].data['title'] for i in xrange(len(validData))]
 validSalary = [validData[i].data['SalaryMid'] for i in xrange(len(validData))]
-validTests = getTestData(validData,0,len(validData),vect,vect2)
+validTests = getTestData(validData,0,len(validData),vect1,vect2,vect3,vect4,vect5,vect6,vect7,vect8)
 
 print 'predicting'
 predictions = np.exp(model.predict(validTests))
--- regession/train.py
+++ regession/train.py
@@ -28,9 +28,9 @@ def fit(merged,salary):
 	#model = linear_model.LinearRegression()
 	#model = svm.SVR(kernel='rbf', C=1e3, gamma=0.1)
 	#model = neighbors.KNeighborsRegressor(n_neighbors=19)
-	#model = tree.DecisionTreeRegressor()
+	model = tree.DecisionTreeRegressor()
 	#model = neural_network.MLPRegressor(hidden_layer_sizes=(50, ))
-	model = linear_model.BayesianRidge()
+	#model = linear_model.BayesianRidge()
 	model.fit(merged,salary)
 	print 'Train model done in',time()-tModel,'seconds'
 	return model
@@ -41,20 +41,48 @@ def TokenizingData(jobs):
 	salary = []
 	des = []
 	titles = []
+	level = []
+	org = []
+	lo = []
+	edu = []
+	exp = []
+	emp = []
+	ind = []
+	jobfunc = []
 	for i in xrange(trainNum):
 		job = jobs[i].data
-		salary.append(np.log(float(job['SalaryMid'])))
-		des.append(job['level'] + ' ' + job['organization'] + ' ' + job['joblocation'] + ' ' + job['education'] + ' ' + 
-		job['experience'] + ' ' + job['employmentType'] + ' ' + job['industry'] + ' ' + job['jobfunction'])
 		titles.append(job['title'])
+		level.append(job['level'])
+		org.append(job['organization'])
+		lo.append(job['joblocation'])
+		edu.append(job['education'])
+		exp.append(job['experience'])
+		ind.append(job['industry'])
+		jobfunc.append(job['jobfunction'])
+		salary.append(np.log(float(job['SalaryMid'])))
+		#des.append(job['level'] + ' ' + job['organization'] + ' ' + job['joblocation'] + ' ' + job['education'] + ' ' + 
+		#job['experience'] + ' ' + job['employmentType'] + ' ' + job['industry'] + ' ' + job['jobfunction'])		
 	print 'Tokenizing'
-	vect = TfidfVectorizer(min_df=1,ngram_range=(1,3),max_features=24000000)
+	vect1 = TfidfVectorizer(min_df=1,ngram_range=(1,3),max_features=24000000)
 	vect2 = TfidfVectorizer(min_df=1,ngram_range=(1,3),max_features=24000000)
-	jobtitles = vect.fit_transform(titles)
-	descrip = vect2.fit_transform(des)	
-	merged = hstack((jobtitles,descrip))
+	vect3 = TfidfVectorizer(min_df=1,ngram_range=(1,3),max_features=24000000)
+	vect4 = TfidfVectorizer(min_df=1,ngram_range=(1,3),max_features=24000000)
+	vect5 = TfidfVectorizer(min_df=1,ngram_range=(1,3),max_features=24000000)
+	vect6 = TfidfVectorizer(min_df=1,ngram_range=(1,3),max_features=24000000)
+	vect7 = TfidfVectorizer(min_df=1,ngram_range=(1,3),max_features=24000000)
+	vect8 = TfidfVectorizer(min_df=1,ngram_range=(1,3),max_features=24000000)
+	jobtitles = vect1.fit_transform(titles)
+	#descrip = vect2.fit_transform(des)
+	joblevel = vect2.fit_transform(level)
+	joborg = vect3.fit_transform(org)
+	joblo = vect4.fit_transform(lo)
+	jobedu = vect5.fit_transform(edu)
+	jobexp = vect6.fit_transform(exp)
+	jobind = vect7.fit_transform(ind)
+	jobfunction = vect8.fit_transform(jobfunc)	
+	merged = hstack((jobtitles,joblevel,joborg,joblo,jobedu,jobexp,jobind,jobfunction))
 	print 'Tokenized job data in',time()-tToken,'seconds'
-	return merged,salary,vect,vect2
+	return merged,salary,vect1,vect2,vect3,vect4,vect5,vect6,vect7,vect8
 
 
 paths = json.load(open('SETTINGS.json','rb'))
@@ -62,10 +90,16 @@ t0 = time()
 print 'Loading data from',paths['TRAIN_DATA_PATH']
 jobs = LoadData(paths['TRAIN_DATA_PATH'])
 trainNum = len(jobs)
-merged,salary,vect,vect2= TokenizingData(jobs)
+merged,salary,vect1,vect2,vect3,vect4,vect5,vect6,vect7,vect8= TokenizingData(jobs)
 print 'Traning model'
 model = fit(merged,salary)
 print 'Save model as',paths['MODEL_PATH']
 joblib.dump(model,paths['MODEL_PATH'],compress=3)
-joblib.dump(vect,paths['VECTORIZER_1_PATH'],compress=3)
+joblib.dump(vect1,paths['VECTORIZER_1_PATH'],compress=3)
 joblib.dump(vect2,paths['VECTORIZER_2_PATH'],compress=3)
+joblib.dump(vect3,paths['VECTORIZER_3_PATH'],compress=3)
+joblib.dump(vect4,paths['VECTORIZER_4_PATH'],compress=3)
+joblib.dump(vect5,paths['VECTORIZER_5_PATH'],compress=3)
+joblib.dump(vect6,paths['VECTORIZER_6_PATH'],compress=3)
+joblib.dump(vect7,paths['VECTORIZER_7_PATH'],compress=3)
+joblib.dump(vect8,paths['VECTORIZER_8_PATH'],compress=3)
